fc747e1702e7bfdd764367de808790eb60cffa98
Roni Jacobson <ronipony@gmail.com>
2020-05-31 00:16:39 +0300

    add lame and setup jni

--- android/src/main/jni/lame_native.c
+++ android/src/main/jni/lame_native.c
@@ -4,48 +4,18 @@
 
 #include <string.h>
 #include <jni.h>
+#include "libmp3lame/lame.h"
 
-/* This is a trivial JNI example where we use a native method
- * to return a new VM String. See the corresponding Java source
- * file located at:
- *
- *   apps/samples/hello-jni/project/src/com/example/hellojni/HelloJni.java
- */
-JNIEXPORT jstring JNICALL
-Java_com_jacobson_flutter_1recording_MP3Lame_stringFromJNI( JNIEnv* env,
-                                                  jobject thiz )
-{
-#if defined(__arm__)
-  #if defined(__ARM_ARCH_7A__)
-    #if defined(__ARM_NEON__)
-      #if defined(__ARM_PCS_VFP)
-        #define ABI "armeabi-v7a/NEON (hard-float)"
-      #else
-        #define ABI "armeabi-v7a/NEON"
-      #endif
-    #else
-      #if defined(__ARM_PCS_VFP)
-        #define ABI "armeabi-v7a (hard-float)"
-      #else
-        #define ABI "armeabi-v7a"
-      #endif
-    #endif
-  #else
-   #define ABI "armeabi"
-  #endif
-#elif defined(__i386__)
-   #define ABI "x86"
-#elif defined(__x86_64__)
-   #define ABI "x86_64"
-#elif defined(__mips64)  /* mips64el-* toolchain defines __mips__ too */
-   #define ABI "mips64"
-#elif defined(__mips__)
-   #define ABI "mips"
-#elif defined(__aarch64__)
-   #define ABI "arm64-v8a"
-#else
-   #define ABI "unknown"
-#endif
-
-    return (*env)->NewStringUTF(env, "Hello from JNI !  Compiled with ABI " ABI ".");
+lame_global_flags *lame_flags;
+JNIEXPORT void JNICALL
+Java_com_jacobson_flutter_1recording_MP3Lame_initialize(JNIEnv *env, jobject thiz, jint bit_rate,
+                                                        jint sample_rate, jint lame_quality) {
+    lame_flags = lame_init();
+    lame_set_in_samplerate(lame_flags, sample_rate);
+    lame_set_out_samplerate(lame_flags, sample_rate);
+    lame_set_brate(lame_flags, bit_rate);
+    lame_set_quality(lame_flags, lame_quality);
+    lame_set_num_channels(lame_flags, 1);
+    lame_set_mode(lame_flags, MONO);
+    lame_init_params(lame_flags);
 }
