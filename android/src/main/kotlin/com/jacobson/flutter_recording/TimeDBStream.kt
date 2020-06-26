3b8b7ebc8d26cb53915477563ea30f44c1036e63
Roni Jacobson <ronipony@gmail.com>
2020-06-26 14:45:12 +0300

    add a singleton for communication back to flutter

---
+++ android/src/main/kotlin/com/jacobson/flutter_recording/TimeDBStream.kt
@@ -0,0 +1,25 @@
+package com.jacobson.flutter_recording
+
+import android.os.Handler
+import android.os.Looper
+import io.flutter.plugin.common.EventChannel
+import java.time.Instant
+import java.util.*
+
+object TimeDBStream: EventChannel.StreamHandler {
+    var events: EventChannel.EventSink? = null
+
+    override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+        this.events = events
+    }
+
+    fun sendInfo(timestamp: Int, maxDecibel: Int) {
+        Handler(Looper.getMainLooper()).post {
+            events?.success(arrayListOf(timestamp, maxDecibel))
+        }
+    }
+
+    override fun onCancel(arguments: Any?) {
+        return
+    }
+}
