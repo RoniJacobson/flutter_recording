30036f7f75e82aae5efa3a7a0e01ecddeb33fc90
Roni Jacobson <ronipony@gmail.com>
2020-07-19 16:57:38 +0300

    add a first implementation of the recording of the built in android formats

---
+++ android/src/main/kotlin/com/jacobson/flutter_recording/GeneralRecorder.kt
@@ -0,0 +1,47 @@
+package com.jacobson.flutter_recording
+
+import android.media.MediaMuxer
+import android.media.MediaRecorder
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.io.File
+import kotlin.concurrent.timerTask
+
+class GeneralRecorder(fileName: String, encoder: Int, outputFormat: Int, bitRate: Int, sampleRate: Int, notificationCallback: (name: String) -> Unit, callbackRate: Long, timestampBufferLength: Int) : RecordingSuper(fileName, bitRate, sampleRate, notificationCallback, callbackRate, timestampBufferLength) {
+
+    val recorder: MediaRecorder = MediaRecorder()
+    init {
+        recorder.setAudioChannels(1)
+        recorder.setAudioEncoder(encoder)
+        recorder.setOutputFormat(outputFormat)
+        recorder.setAudioEncodingBitRate(bitRate)
+        recorder.setAudioSamplingRate(sampleRate)
+        val file = File(fileName)
+        file.parentFile.mkdirs()
+        recorder.setOutputFile(fileName)
+    }
+    override fun startRecording() {
+        recorder.start()
+        infoTimer.scheduleAtFixedRate(timerTask { timerFunction() }, callbackRate - (pauseTime - lastTimerTick), callbackRate)
+    }
+
+    override fun stopRecording() {
+        recorder.stop()
+    }
+
+    override fun getMaxAmplitude(): Double {
+        return recorder.maxAmplitude.toDouble()
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun pauseRecording() {
+        recorder.pause()
+        super.pauseRecording()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun resumeRecoding() {
+        recorder.resume()
+    }
+}
